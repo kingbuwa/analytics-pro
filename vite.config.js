7f50c650f6a8e880947e16606f90ed4a64e58c71
kingbuwa <buwaking@gmail.com>
2025-09-25 20:56:57 +0100

    Fix router issue, clean up App.jsx and main.jsx, and make dashboard load correctly

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/analytics-pro/", // 👈 needed for GH Pages
   plugins: [react()],
-})
+});
+
